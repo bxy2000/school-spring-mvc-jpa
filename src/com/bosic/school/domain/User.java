583171de4abb7cf6fd6a1fbfd50ab07b19c44bb7
bxy2000 <bxy_2000@163.com>
2017-06-28 00:55:35 +0800

    更新实体类

--- src/com/bosic/school/domain/User.java
+++ src/com/bosic/school/domain/User.java
@@ -10,13 +10,13 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="user", catalog="test")
+@Table(name="users")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	@Column
+	@Column(columnDefinition="user_name")
 	private String username;
 	@Column
 	private String password;
