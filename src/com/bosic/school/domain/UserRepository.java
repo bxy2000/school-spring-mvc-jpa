cd3da19519eb43334fd345144965df106ae87a4f
bxy2000 <bxy_2000@163.com>
2017-06-28 00:58:55 +0800

    清除用户相关数据

--- src/com/bosic/school/domain/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bosic.school.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-	User findUserByUsername(String username);
-}
-
-
